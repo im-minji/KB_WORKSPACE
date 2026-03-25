6b76a0f011d461c18768ad9f5b4fedfa32f46a36
“im-minji” <mjlim514@gmail.com>
2026-03-25 09:18:33 +0900

    vue.js 컴포넌트 20260324

---
+++ fullstack/01_WEB/05_VUE/chapter07/test-vue-app/src/components/CheckboxItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul>
+    <li><input type="checkbox" v-model="checked" /> 옵션1</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "CheckboxItem",
+  data() {
+    return {
+      checked: false,
+    };
+  },
+};
+</script>
