f17f807411815eaae3f1b2d07785efd91e80f2fa
“im-minji” <mjlim514@gmail.com>
2026-03-25 09:52:38 +0900

    KB practice mv

---
+++ fullstack/KB_workspace/vue-project/src/components/ProductItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <tr>
+    <td>이미지자리</td>
+    <td>{{ childProduct.prodNo }}</td>
+    <td>{{ childProduct.prodName }}</td>
+    <td>{{ childProduct.prodPrice }}</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "ProductItem",
+  props: {
+    childProduct: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
